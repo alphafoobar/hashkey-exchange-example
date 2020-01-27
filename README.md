f6a0ad179f244038235302e3302639d1c7eb5cfe
james <alphafoobar@users.noreply.github.com>
2020-01-28 10:54:39 +1300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Example Hashkey websocket implementation in NodeJs
+
+Based on Sample codes available here in [Hashkey sample code](https://hashkeypro.github.io/api-spec/#4-sample-code).
---
+++ example.js
@@ -0,0 +1,21 @@
+const WebSocket = require('ws');
+const ws = new WebSocket('wss://pro.hashkey.com/APITradeWS/v1/messages', {
+    checkServerIdentity: () => {}
+});
+const data = "WSS/APITradeWS/v1/messages";
+
+ws.on('open', ()=>{
+    console.log('WebSocket open')
+    ws.send('{"type":"subscribe", "channel":{"ticker":["ETH-BTC"]}}');
+    ws.send('{"type":"subscribe", "channel":{"level2":["ETH-BTC"]}}');
+});
+
+//Send ping
+//发送 ping
+setInterval(() => {
+    ws.ping('');
+}, 15000);
+
+ws.on('message', (data) => {
+    console.log(data);
+});
